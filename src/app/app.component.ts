07781359319c4c62333d59dce7e47fbb47f0f868
Louis Sutton <louis.sutton.iii@gmail.com>
2020-03-05 15:35:05 -0800

    Added Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-v8';
+export class AppComponent implements OnInit {
+  title = 'Feature Im Working On';
+  constructor(private fb: FormBuilder, private fc: FormControl){}
+  myForm: FormGroup;
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['Benedict', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
+      message: ['Louis', [Validators.required, Validators.minLength(5)]]
+    });
+    }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Message', form.value.message);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,9 +17,11 @@ import { TranslocoRootModule } from './transloco-root.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    TranslocoRootModule
+    TranslocoRootModule,
+    ReactiveFormsModule,
+    
   ],
-  providers: [],
+  providers: [FormControl, Validators],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
