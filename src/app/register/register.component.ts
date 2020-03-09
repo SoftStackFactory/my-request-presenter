52a4d72799fc15f34ae30cfbf922d8c5cc8b585f
Rae <raemdonaldson@gmail.com>
2020-03-08 18:08:30 -0700

    login and register created

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      DOB: ['', [Validators.required, Validators.minLength(8), Validators.pattern('[0-9]*')]],
+    });
   }
 
 }
