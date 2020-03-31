78ce548926b934e6d0a33e97defea1989c17152a
Rae <raemdonaldson@gmail.com>
2020-03-30 21:58:08 -0700

    content page started, hard coded image

--- src/app/components/admin-form-create-event/admin-form-create-event.component.ts
+++ src/app/components/admin-form-create-event/admin-form-create-event.component.ts
@@ -16,6 +16,14 @@ export class AdminFormCreateEventComponent implements OnInit {
   freqMin = 10;
   displayMax = 120;
   freqMax = 600;
+  images = [
+    { src : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/colorful-of-dahlia-pink-flower-in-beautiful-garden-royalty-free-image-825886130-1554743243.jpg?crop=0.669xw:1.00xh;0.331xw,0&resize=640:*"
+    },
+    {
+      src : "https://www.gardendesign.com/pictures/images/263x300Exact_62x0/site_3/helianthus-yellow-flower-pixabay_11863.jpg"
+    }
+  ]
+
 
   constructor(private _formBuilder: FormBuilder, private _router: Router ) { }
 
