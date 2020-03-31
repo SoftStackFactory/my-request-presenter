88d1d080446ba7c346c86ff61443228815d1171c
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-03-30 19:04:50 -0700

    save point

--- src/app/components/next-up/next-up.component.ts
+++ src/app/components/next-up/next-up.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./next-up.component.scss']
 })
 export class NextUpComponent implements OnInit {
+  // @Input() artist: string;
+  // @Input() song: string;
+  // @Input() amount: number;
+  // @Input() memo: string;
+
+  // hard coded for now. 
+  song = 'iusto qui accusantium';
+  artist = 'Richard Port Velvahaven';
+  memo = 'Illum consectetur ullam quasi.';
+  amount = 30.33;
 
   constructor() { }
 
