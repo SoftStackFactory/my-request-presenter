e278edb88ba474dde9479a7bd040126bfe9dddfb
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-03-30 19:43:52 -0700

    add hard coded input

--- src/app/components/now-playing/now-playing.component.ts
+++ src/app/components/now-playing/now-playing.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NowPlayingComponent implements OnInit {
 
+  // @Input() artist: string;
+  // @Input() song: string;
+  // @Input() amount: number;
+  // @Input() memo: string;
+
+  // hard coded for now. 
+  song = 'iusto qui accusantium';
+  artist = 'Richard Port Velvahaven';
+  memo = 'Illum consectetur ullam quasi.';
+  amount = 30.33;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
