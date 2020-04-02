981499a6cf7b2df14934d84c230c475fd9ca8457
Cherr Batac <ch3rr17>
2020-04-01 21:45:40 -0700

    removes unnecessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { MainListComponent } from './components/main-list/main-list.component';
 
 
