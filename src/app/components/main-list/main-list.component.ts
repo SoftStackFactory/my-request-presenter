7b5cfad1f7ba786d74a83859417ccb5f788c5f28
Cherr Batac <ch3rr17>
2020-04-12 11:14:49 -0700

    adds revised ads

--- src/app/components/main-list/main-list.component.ts
+++ src/app/components/main-list/main-list.component.ts
@@ -35,19 +35,35 @@ songs = [{
   constructor() { }
 
   ngOnInit() {
-    if( this.songs[0]) { 
-        this.songList.push(this.songs[0]);
-    }
-    this.timer = setInterval(() => {
-        if (this.index < this.songs.length) {
-           var slist = this.songList.push(this.songs[this.index]);
-           this.index++;
-           console.log('ads',this.songs)
+    // if( this.songs[0]) { 
+    //     this.songList.push(this.songs[0]);
+    // } 
+    // this.timer = setInterval(() => {
+    //     if (this.index < this.songs.length) {
+    //        var slist = this.songList.push(this.songs[this.index]);
+    //        this.index++;
+    //        console.log('ads',this.songs)
            
-        } else { 
-           clearInterval(this.timer);
+    //     } else { 
+    //        clearInterval(this.timer);
+    //     }
+    //  }, 4000)
+    
+    this.timer = setInterval(() => {
+      //this is just a filler action.  We would actually so something like
+      // this.adUrl = this.ads[this.index].url
+        console.log(this.songs[this.index].name)
+        if (this.index === this.songs.length -1) {
+        // if we are at the end of the list start again at the beginning;
+          this.index = 0;
+        } else {
+        // else go to next item
+          this.index ++;
         }
-     }, 4000)
+      
+    }, 4000)
+
+
   }
 
   // getEvents() {
