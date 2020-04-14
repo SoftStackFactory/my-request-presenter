464eb52f508136347a8c41bb4accf5cc22922db4
Cherr Batac <ch3rr17>
2020-04-13 20:28:58 -0700

    removes double ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,13 @@ import { MainListComponent } from './components/main-list/main-list.component';
 import { AdminFormCreateEventComponent } from './components/admin-form-create-event/admin-form-create-event.component';
 import { MatButtonModule, MatTabsModule, MatStepperModule, MatFormFieldModule, MatSliderModule } from '@angular/material';
 import { AdminFormManageEventComponent } from './components/admin-form-manage-event/admin-form-manage-event.component';
-import { DragDropModule } from '@angular/cdk/drag-drop'
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainListComponent
-
-
-@NgModule({
-  declarations: [ 
+    MainListComponent,
     AppComponent,
     AdminFormCreateEventComponent,
     AdminFormManageEventComponent
