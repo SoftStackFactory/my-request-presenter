d95616d56fdb6230945dc88ce09983d39d4a7d79
Cherr Batac <ch3rr17>
2020-04-19 12:02:02 -0700

    adds song view and ad logic, modifes adsjson file to modify img list

--- src/app/components/main-list/main-list.component.ts
+++ src/app/components/main-list/main-list.component.ts
@@ -10,18 +10,30 @@ export class MainListComponent implements OnInit {
 
   timer: any;
   adUrl: any;
+  songName: any;
+  ads: any;
 
   songList=[];
   adList = [];
-  index = 1;
+  index1 = 1;
+  index2 = 1;
   songs = [{
     name: 'Varona',
-    artist: 'Geographer'
+    artist: 'Geographer',
+    imgUrl: 'assets/img/ad1.jpg'
   },
   {
     name: 'Kite',
-    artist: 'Geographer'
-  }]
+    artist: 'Geographer',
+    imgUrl: 'assets/img/ad2.jpg'
+  },
+  {
+    name: 'Need Me',
+    artist: 'Geographer',
+    imgUrl: 'assets/img/ad3.jpg'
+  }
+
+]
 
 
 
@@ -32,18 +44,19 @@ export class MainListComponent implements OnInit {
     this.adsService.getAds().subscribe((ads: any[]) => {
       console.log('ADS',ads)
       // this.adList = ads[0].adImage1;
-      this.adUrl = ads[0].adImage1;
-      console.log('ad url', this.adList);
+      // this.adUrl = ads[0].adImages[1]; //this works
+      // this.adUrl = this.ads[this.index1];
+      // console.log('ad url', this.adUrl);
       this.timer = setInterval(() => {
         console.log('ADLIST');
-        this.adUrl = this.adList[this.index]
+        this.adUrl = this.ads[this.index1].adImages;
         console.log('list here',this.adUrl);
-        if(this.index === this.songs.length -1){
-          this.index = 0
+        if(this.index1 === this.songs.length -1){
+          this.index1 = 0
         } else {
-          this.index++;
+          this.index1++;
         }
-      }, ads[0].duration * 1000);
+      }, ads[this.index1].duration * 4000);
     })
     // this.adsService.getAds().subscribe((ads: any[]) => {
     //   console.log('ADS',ads);
@@ -52,17 +65,18 @@ export class MainListComponent implements OnInit {
     this.timer = setInterval(() => {
       //this is just a filler action.  We would actually so something like
       // this.adUrl = this.ads[this.index].url
-        console.log(this.songs[this.index].name)
-        if (this.index === this.songs.length -1) {
+      this.songName = this.songs[this.index2].name; //this works and rotates the name properly
+        console.log('SONG: ', this.songName)
+        if (this.index2 === this.songs.length -1) {
         // if we are at the end of the list start again at the beginning;
-          this.index = 0;
+          this.index2 = 0;
         } else {
         // else go to next item
-          this.index ++;
+          this.index2 ++;
         }
     }, 4000)
 
-
+    
   }
 
   // getEvents() {
